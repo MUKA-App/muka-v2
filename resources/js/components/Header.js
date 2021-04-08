af527da3442c9413451afd1cbd603d2abb0439ad
Peter Foris <peterforis6@gmail.com>
2021-04-08 14:11:01 +0200

    Make links private or public

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -2,16 +2,19 @@ import {Link} from "react-router-dom";
 import React from "react";
 import logo from '../../../public/images/header_muka_logo.png';
 import '../../sass/header.scss';
+import { isLogin } from '../auth/auth';
 
 export default function Header(props) {
     return (
         <div className={"headerContainer"}>
             <img src={logo} className={"headerLogo"} alt="Logo"/>
-            <Link to={'/home'} className={"generalLink"}>Home</Link>
-            <Link to={'/about'} className={"generalLink"}>About</Link>
-            <Link to={'/sponsors'} className={"generalLink"}>Sponsors</Link>
-            <Link to={'/login'} className={"loginLink"}>Login</Link>
-            <Link to={'/register'} className={"joinLink"}>Join Us</Link>
+            {!isLogin() && <Link to={'/home'} className={"generalLink"}>Home</Link>}
+            {!isLogin() && <Link to={'/about'} className={"generalLink"}>About</Link>}
+            {!isLogin() && <Link to={'/sponsors'} className={"generalLink"}>Sponsors</Link>}
+            {!isLogin() && <Link to={'/login'} className={"loginLink"}>Login</Link>}
+            {!isLogin() && <Link to={'/register'} className={"joinLink"}>Join Us</Link>}
+            {isLogin() && <Link to={'/profile'} className={"generalLink"}>Profile</Link>}
+            {isLogin() && <Link to={'/dashboard'} className={"generalLink"}>Dashboard</Link>}
         </div>
     );
 }
