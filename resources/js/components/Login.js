878c285bf2d204e3662ac1e906c9721b2f412e5f
Peter Foris <peterforis6@gmail.com>
2021-04-09 11:14:30 +0200

    Add toastify to Login and Register

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -1,10 +1,11 @@
-import '../../sass/app.scss';
 import '../../sass/login.scss';
 import React, {useState} from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import hugeImg from '/images/huge_muka_logo.png';
 import axios from "axios";
 import {withRouter} from "react-router-dom";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login(props) {
 
@@ -14,17 +15,14 @@ function Login(props) {
         remember: false
     });
 
+    const notify = (text) => toast(text);
+
     const handleChange = (e) => {
-        const {id, value} = e.target;
+        const id = e.target.name;
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
         setState(prevState => ({
             ...prevState,
             [id]: value
-        }))
-    };
-    const toggle = (e) => {
-        setState(prevState => ({
-            ...prevState,
-            remember: !prevState.remember
         }))
     };
 
@@ -33,84 +31,109 @@ function Login(props) {
         const payload = {
             "email": state.email,
             "password": state.password,
-            "remember_token": true
+            "remember_token": state.remember
         };
 
         axios.post(process.env.MIX_APP_BASE_URL + "/login", payload)
             .then(function (response) {
-                    if (response.status === 200) {
-                        console.log("Successful login.");
-                        localStorage.setItem('auth', 'true');
-                        props.history.push('/dashboard');
-                    } else {
-                        console.log("Other error occurred with code: " + response.status);
+                    switch (response.status) {
+                        case 200:
+                            localStorage.setItem('auth', 'true');
+                            props.history.push('/dashboard');
+                            break;
+                        case 401:
+                            notify("Incorrect password given!");
+                            break;
+                        case 422:
+                            notify("The password must be at least 8 characters!");
+                            break;
+                        default:
+                            notify("Error occurred with code: " + response.status);
                     }
                 }
             )
             .catch(function (error) {
-                console.log(error);
+                notify(error.response.data.message);
             });
     };
 
     const handleSubmitClick = (e) => {
         e.preventDefault();
-        // TODO: Some validation to be added
-        sendDetailsToServer();
+        if (state.email === "") {
+            notify("You must use an email address!");
+        } else if (state.password.length < 8) {
+            notify("The password must be at least 8 characters!")
+        } else {
+            sendDetailsToServer();
+        }
     };
 
     return (
-        <Container>
-            <Row>
-                <Col
-                    className={"halfColumn col-sm pt-lg-6 pt-md-3 pt-sm-1 pt-0 pr-lg-5 pr-xl-6 justify-content-center align-items-center"}>
-                    <div className={" vertical-center"}>
-                        <h1 className={"welcomeText"}>Welcome</h1>
-                        <img className={"loginPhoto"} src={hugeImg}/>
-                        <h5 className={"joinText"}>Join MUKA now and find people with the same passion as you!</h5>
-                    </div>
-                </Col>
-                <Col>
-                    <div className={"bottomdiv"}>
-                        <form>
-                            <Row className={"spacedRow"}>
-                                <input type="email"
-                                       name="email"
-                                       id="email"
-                                       className="form-control login-input"
-                                       value={state.email}
-                                       onChange={handleChange}
-                                       required
-                                       placeholder="Email e.g. john.doe@example.com"/>
-                            </Row>
-                            <Row className={"spacedRow"}>
-                                <input type="password"
-                                       name="password"
-                                       id="password"
-                                       className="form-control login-input"
-                                       value={state.password}
-                                       onChange={handleChange}
-                                       required
-                                       placeholder="Password"/>
-                            </Row>
-                            <Row>
-                            <input
-                                name="checkbox"
-                                type="checkbox"
-                                checked={state.remember}
-                                onChange={toggle}
-                            />
-                                <label htmlFor="checkbox"> I want to stay logged in</label>
-                            </Row>
-                            <Row className={"spacedRow"}>
-                                <button className="btn btn-lg login-btn" type="submit" onClick={handleSubmitClick}>
-                                    Sign in
-                                </button>
-                            </Row>
-                        </form>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <div>
+            <Container>
+                <Row>
+                    <Col
+                        className={"halfColumn col-sm pt-lg-6 pt-md-3 pt-sm-1 pt-0 pr-lg-5 pr-xl-6 justify-content-center align-items-center"}>
+                        <div className={" vertical-center"}>
+                            <h1 className={"welcomeText"}>Welcome</h1>
+                            <img className={"loginPhoto"} src={hugeImg}/>
+                            <h5 className={"joinText"}>Join MUKA now and find people with the same passion as you!</h5>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className={"bottomdiv"}>
+                            <form>
+                                <Row className={"spacedRow"}>
+                                    <input type="email"
+                                           name="email"
+                                           className="form-control login-input"
+                                           value={state.email}
+                                           onChange={handleChange}
+                                           required
+                                           placeholder="Email e.g. john.doe@example.com"/>
+                                </Row>
+                                <Row className={"spacedRow"}>
+                                    <input type="password"
+                                           name="password"
+                                           className="form-control login-input"
+                                           value={state.password}
+                                           onChange={handleChange}
+                                           required
+                                           placeholder="Password"/>
+                                </Row>
+                                <Row>
+                                    <input
+                                        type="checkbox"
+                                        name="remember"
+                                        checked={state.remember}
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="checkbox"> I want to stay logged in</label>
+                                </Row>
+                                <Row className={"spacedRow"}>
+                                    <button className="btn btn-lg login-btn" type="submit" onClick={handleSubmitClick}>
+                                        Sign in
+                                    </button>
+                                </Row>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+
+        </div>
     )
 }
 
--- resources/js/components/Register.js
+++ resources/js/components/Register.js
@@ -5,6 +5,8 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import {withRouter} from "react-router-dom";
 import {Container, Row, Col} from 'reactstrap';
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register(props) {
 
@@ -15,6 +17,8 @@ function Register(props) {
         checkedTC: false
     });
 
+    const notify = (text) => toast(text);
+
     const handleChange = (e) => {
         const id = e.target.name;
         const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
@@ -22,6 +26,7 @@ function Register(props) {
             ...prevState,
             [id]: value
         }))
+        console.log(state);
     };
 
     const sendDetailsToServer = () => {
@@ -35,108 +40,122 @@ function Register(props) {
             .then(function (response) {
                     switch (response.status) {
                         case 201:
-                            console.log("Successful registration.");
                             props.history.push('/register/confirm');
                             break;
                         case 409:
-                            console.log("User already exists.");
+                            notify("User already exists!");
                             break;
                         case 422:
-                            console.log("Invalid credentials");
+                            notify("Invalid credentials!");
                             break;
                         case 400:
                         case 500:
-                            console.log("Server error.");
+                            notify("Server error!");
                             break;
                         default:
-                            console.log("Other error occurred with code: " + response.status);
+                            notify("Other error occurred with code: " + response.status);
                     }
                 }
             )
             .catch(function (error) {
-                console.log(error);
+                notify(error);
             });
     };
 
     const handleSubmitClick = (e) => {
 
         e.preventDefault();
-
-        if (state.password !== state.confirmPassword) {
-            console.log("Non matching passwords.");
+        if (state.email === "") {
+            notify("You must add an email address!");
+        } else if (state.password !== state.confirmPassword) {
+            notify("Password and Confirm Password must be the same!");
         } else if (state.password.length < 8) {
-            console.log("Password length < 8.")
-        } else if (!state.checked) {
-            console.log("Terms and Conditions not accepted")
-        }else{
+            notify("The password must be at least 8 characters!")
+        } else if (!state.checkedTC) {
+            notify("Terms and Conditions must be accepted!");
+        } else {
             sendDetailsToServer();
         }
     };
 
     return (
-        <Container>
-            <Row>
-                <Col
-                    className={"halfColumn col-sm pt-lg-6 pt-md-3 pt-sm-1 pt-0 pr-lg-5 pr-xl-6 justify-content-center align-items-center"}>
-                    <div className={" vertical-center"}>
-                        <h1 className={"welcomeText"}>Welcome</h1>
-                        <img className={"loginPhoto"} src={hugeImg}/>
-                        <h5 className={"joinText"}>Join MUKA now and find people with the same passion as you!</h5>
-                    </div>
-                </Col>
-                <Col>
-                    <div className={"bottomdiv"}>
-                        <form>
-                            <Row className={"spacedRow"}>
-                                <input type="email"
-                                       name="email"
-                                       id="email"
-                                       className="form-control login-input"
-                                       value={state.email}
-                                       onChange={handleChange}
-                                       required
-                                       placeholder="Email e.g. john.doe@example.com"/>
-                            </Row>
-                            <Row className={"spacedRow"}>
-                                <input type="password"
-                                       name="password"
-                                       id="password"
-                                       className="form-control login-input"
-                                       value={state.password}
-                                       onChange={handleChange}
-                                       required
-                                       placeholder="Password"/>
-                            </Row>
-                            <Row className={"spacedRow"}>
-                                <input type="password"
-                                       name="password"
-                                       id="confirmPassword"
-                                       className="form-control login-input"
-                                       value={state.confirmPassword}
-                                       onChange={handleChange}
-                                       required
-                                       placeholder="Confirm Password"/>
-                            </Row>
-                            <Row>
-                                <input
-                                    type="checkbox"
-                                    name="checkedTC"
-                                    checked={state.checked}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <label htmlFor="checkbox"> Accept the Terms and Conditions</label>
-                            </Row>
-                            <Row className={"spacedRow"}>
-                                <button className="btn btn-lg login-btn" type="submit" onClick={handleSubmitClick}>
-                                    Register
-                                </button>
-                            </Row>
-                        </form>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <div>
+            <Container>
+                <Row>
+                    <Col
+                        className={"halfColumn col-sm pt-lg-6 pt-md-3 pt-sm-1 pt-0 pr-lg-5 pr-xl-6 justify-content-center align-items-center"}>
+                        <div className={" vertical-center"}>
+                            <h1 className={"welcomeText"}>Welcome</h1>
+                            <img className={"loginPhoto"} src={hugeImg}/>
+                            <h5 className={"joinText"}>Join MUKA now and find people with the same passion as you!</h5>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className={"bottomdiv"}>
+                            <form>
+                                <Row className={"spacedRow"}>
+                                    <input type="email"
+                                           name="email"
+                                           id="email"
+                                           className="form-control login-input"
+                                           value={state.email}
+                                           onChange={handleChange}
+                                           required
+                                           placeholder="Email e.g. john.doe@example.com"/>
+                                </Row>
+                                <Row className={"spacedRow"}>
+                                    <input type="password"
+                                           name="password"
+                                           id="password"
+                                           className="form-control login-input"
+                                           value={state.password}
+                                           onChange={handleChange}
+                                           required
+                                           placeholder="Password"/>
+                                </Row>
+                                <Row className={"spacedRow"}>
+                                    <input type="password"
+                                           name="confirmPassword"
+                                           id="confirmPassword"
+                                           className="form-control login-input"
+                                           value={state.confirmPassword}
+                                           onChange={handleChange}
+                                           required
+                                           placeholder="Confirm Password"/>
+                                </Row>
+                                <Row>
+                                    <input
+                                        type="checkbox"
+                                        name="checkedTC"
+                                        checked={state.checkedTC}
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="checkbox"> Accept the Terms and Conditions</label>
+                                </Row>
+                                <Row className={"spacedRow"}>
+                                    <button className="btn btn-lg login-btn" type="submit" onClick={handleSubmitClick}>
+                                        Register
+                                    </button>
+                                </Row>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+
+        </div>
     )
 }
 
