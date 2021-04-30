58c9376d71743c662456d2ae8f06cb36bb10c4d9
Mark Paveszka <mark.paveszka@gmail.com>
2021-04-30 15:56:00 +0100

    Added logout method

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ReactPageController::class, 'index']);
+Route::get('/', [ReactPageController::class, 'index'])->name('home');
 Route::get('/login', [ReactPageController::class, 'index'])->name('login');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::post('/login', [LoginController::class, 'login']);
 
