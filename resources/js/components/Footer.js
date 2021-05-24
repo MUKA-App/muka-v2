af450a4677d550e44ce74e5790d8e9060b6f3034
Dawid Lorenz <49237424+TheDawidosDawson@users.noreply.github.com>
2021-05-24 23:09:58 +0200

    Footer (#97)
    * Basic footer code
    
    * Moving footer to the background

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -1,8 +1,11 @@
 import '../../sass/footer.scss';
+import footerImg from '/images/footer_nobg.png';
 
 export default function Footer () {
 
     return (
-        <div className={"footerContainer"}>Footer</div>
+        <div className={"footerContainer"}>
+          <img className={"footer-img"} src={footerImg} width="100%"/>
+        </div>
     )
 }
